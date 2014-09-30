a42b5995dc9e228ae9cf61487c29ed4d2918295a
sohkis <sohkis@gmail.com>
2014-09-30 19:59:05 -0400

    Restore AtomicInteger

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -115,6 +115,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Default launcher application.
